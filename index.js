5979f0315c887e25737bec5680a5f804a53dfa1e
Jonathan Kingston <jonathan@jooped.co.uk>
2015-04-12 20:45:28 +0100

    Adding in AJAX submission demo

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.post('/tests/1/login-submit', function (req, res) {
   res.send('Form submitted. Username: ' + req.body.username + ' Password: ' + req.body.password );
 });
 
+app.post('/tests/2/login-submit', function (req, res) {
+  if (req.body.username === 'user' && req.body.password === 'password') {
+    res.cookie('authdemo', '1qwe2qeqt4t23r23r', { expires: new Date(Date.now() + 900000), httpOnly: true });
+    res.status(200).send({loggedin: true}).end();
+  } else {
+    res.status(400).send({loggedin: false}).end();
+  }
+});
+
 app.use(express.static('public'));
 
 var server = app.listen(app.set('port'), function () {
