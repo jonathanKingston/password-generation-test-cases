2c34a1a77dc1dd08192862f624b9c7817ab89f24
Jonathan Kingston <jonathan@jooped.co.uk>
2015-04-15 00:58:52 +0100

    Adding in form submission cancelation test

---
+++ public/tests/9/script.js
@@ -0,0 +1,37 @@
+(function () {
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function (e) {
+    var ajax = new XMLHttpRequest();
+    var username = this.querySelector('[name=username]').value;
+    var password = this.querySelector('[name=password]').value;
+    var params;
+    e.stopPropagation();
+
+    ajax.onload = ajaxHandle;
+    ajax.onerror = ajaxHandle;
+    ajax.open("post", this.action, true);
+    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    params = 'username=' + username + '&password=' + password;
+    ajax.send(params);
+
+    return false;
+  });
+
+function ajaxHandle(progress) {
+  var responseText = this.responseText;
+  if (this.status >= 200 && this.status < 300) {
+    ajaxSuccess(progress, responseText);
+    return;
+  }
+  ajaxFail(progress, responseText);
+}
+  
+function ajaxSuccess(progress, responseText) {
+  alert('Logged in: ' + responseText);
+}
+function ajaxFail(progress, responseText) {
+  alert('Log in fail: ' + responseText);
+}
+
+}());
